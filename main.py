898f6daaea0de081d4c2f1b5c0086ff2a90eac3f
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-10-12 17:45:08 +0000

    update

--- main.py
+++ main.py
@@ -1,56 +1,31 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
 
-# Title
-st.title("Text Overlay on Image")
+# Function to overlay text on the image
+def overlay_text(image_path, text, position=(10, 10), font_size=20, font_color=(255, 255, 255)):
+    img = Image.open(image_path)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("https://fonts.gstatic.com/s/raleway/v15/1Ptug8zYS_SKggPNyC0IT4ttDfCmxA.woff", font_size)
+    draw.text(position, text, font=font, fill=font_color)
+    return img
 
-# Add the Google Font using HTML and CSS
-st.markdown(
-    """
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
-        .google-font {
-            font-family: 'Raleway', sans-serif;
-            font-size: 24px; /* You can adjust the font size as needed */
-            color: #000000; /* You can specify the font color */
-        }
-    </style>
-    """, unsafe_allow_html=True
-)
+# Streamlit app
+st.title("Image Overlay App")
 
-# Upload an image
-uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+# Upload the image
+image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
-if uploaded_image is not None:
-    # Display the uploaded image
-    st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+# Input field for the overlay text
+text = st.text_input("Enter text for overlay", value="Sample Text")
 
-    # Get user input text
-    text = st.text_input("Enter text to overlay on the image")
+if image is not None:
+    # Display the original image
+    st.image(image, caption="Original Image", use_container_width=True)
 
-    if text:
-        # Open the image using PIL
-        image = Image.open(uploaded_image)
-
-        # Get the image dimensions
-        img_width, img_height = image.size
-
-        # Fixed parameters
-        font_size = 24
-        font_color = "#000000"
-
-        # Load the Raleway font
-        font = ImageFont.load_default()
-
-        # Calculate the position to center the text
-        draw = ImageDraw.Draw(image)
-        text_width, text_height = draw.textsize(text, font)
-
-        text_x = (img_width - text_width) // 2
-        text_y = (img_height - text_height) // 2
-
-        # Draw the text on the image
-        draw.text((text_x, text_y), text, font=font, fill=font_color)
-
-        # Display the image with the overlay
-        st.image(image, caption="Image with Text Overlay", use_column_width=True)
+    # Add overlay text to the image
+    overlayed_image = overlay_text(image, text)
+    
+    # Display the image with overlay
+    st.image(overlayed_image, caption="Image with Overlay", use_container_width=True)
