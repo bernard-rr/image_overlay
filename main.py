20e42e65789ac8447c9701221647940e5cc8d516
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-10-12 17:59:52 +0000

    update

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
-import os
+import io
 
-# Function to overlay text on the image
-def overlay_text(image_path, text, position=(10, 10), font_size=20, font_color=(255, 255, 255), font_path="/workspaces/image_overlay/Raleway/static/Raleway-BoldItalic.ttf"):
-    img = Image.open(image_path)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font_path, font_size)
-    draw.text(position, text, font=font, fill=font_color)
-    return img
+# Load the image
+image_path = 'your_image.jpg'  # Replace with your image path
+image = Image.open(image_path)
 
-# Streamlit app
-st.title("Image Overlay App")
+# Define text to overlay
+text_to_overlay = "Your Text Here"
 
-# Upload the image
-image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+# Define text position (x, y) and other parameters
+text_position = (100, 100)  # Adjust the (x, y) coordinates as needed
+text_color = "white"
+font_path = "/workspaces/image_overlay/Raleway/static/Raleway-BoldItalic.ttf"
+font_size = 36
 
-# Input field for the overlay text
-text = st.text_input("Enter text for overlay", value="Sample Text")
+# Create a drawing context
+draw = ImageDraw.Draw(image)
 
-if image is not None:
-    # Display the original image
-    st.image(image, caption="Original Image")
+# Load the specified font
+font = ImageFont.truetype(font_path, font_size)
 
-    # Add overlay text to the image
-    overlayed_image = overlay_text(image, text)
-    
-    # Display the image with overlay
-    st.image(overlayed_image, caption="Image with Overlay")
+# Draw the text on the image
+draw.text(text_position, text_to_overlay, fill=text_color, font=font)
+
+# Display the modified image in Streamlit
+st.image(image, use_column_width=True)
+
+# Optionally, save the image with the overlay
+image_with_overlay_path = "image_with_overlay.jpg"
+image.save(image_with_overlay_path)
+
+# Display the image with overlay for download
+st.markdown(f"**[Download Image with Overlay]({image_with_overlay_path})**")
